5a7817ca4ccf7d03a655461ebdbd529e31b749f3
Kelly Kahn <kellyakahn@gmail.com>
2013-08-18 12:10:26 -0400

    Final_Project

--- application.js
+++ application.js
@@ -1,24 +1,44 @@
 jQuery(document).ready(function(){
-	console.log("Please Enter a Number Between 1 and 1000 in the field!");
-
-	function Game(){
-		//this section is the constant, not changing, set the weight//
-		weight = Math.floor(1 + Math.random() * 1000);
-	}	
-
-	function guessANum( guess ){
-		if (weight > guess)
-		{
-			console.log("Too low, try again.");
-		}
-		else if (weight < guess)
-		{
-			console.log("Too high, try again.");
-		}
-		else if (weight == guess){
-			console.log("Congratulations. You guessed my Pet elephant's weight!");
-		}
-	}
 	
 
-}
+		weight = 2000 
+		
+
+
+	 TheGuess=function(){
+		var theGuess = $('#MyGuess').find("input").val();
+
+		 if (weight == theGuess){
+			console.log("Congratulations. You guessed my Pet elephant's weight!");
+  		$(".Hot").hide();
+  		$(".Cold").hide(); 
+  		$(".Right").show();
+		}
+
+		else if(theGuess >= 1800 && theGuess <= 2200){ 
+			console.log('You are getting close!');
+  		$(".Cold").hide();
+  		$(".Right").hide(); 
+  		$(".Hot").show();
+  		
+		}
+		
+		else if(theGuess <= 1800 || theGuess >= 2200){ 
+			console.log('Try again!');
+  		$(".Hot").hide();
+  		$(".Right").hide(); 
+  		$(".Cold").show();
+	
+		}
+
+		
+	}
+
+	$('#btnGuess').on('click', function () {
+		TheGuess();
+	});
+
+});
+
+
+
